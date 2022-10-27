c9835954473cf9f7dc965dfc4d8bf2b83183ca36
Sally1005 <1564358705@qq.com>
2022-10-27 10:21:48 +0800

    修改readme.md

--- README.md
+++ README.md
@@ -1,13 +1,44 @@
-# treemenu
+# 项目介绍
+
+该工程为后端项目treemenu-web的前端。
+
+
+# 开发环境
+
+其前端开发环境为vue+element-ui。
+
+项目演示地址：http://localhost:8888
+                      
+
+# 目录结构
+
+```
+# 
+├─.idea        
+├─.public   
+├─src
+│  ├─assets
+│  ├─components
+│  ├─App.vue                   
+│  └─main.js
+├─.gitignore    
+├─babel.config.js
+├─jsconfig.json 
+├─package.json
+├─package-lock.json
+├─README.md
+└─vue.config.js 
+```
+
+
+
+# 项目使用
+
+拿到项目后在Terminal下输入命令 npm run serve 即可启动。
 
 ## Project setup
 ```
 npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
 ```
 
 ### Compiles and minifies for production
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
   <div id="app">
+    <!--搜索框开始-->
+    <el-row>
+      <el-col :span="12">
+        <div class="grid-content bg-purple">
+          <el-input v-model="wd"  placeholder="请输入内容"></el-input>
+        </div>
+      </el-col>
+      <el-col :span="1">
+        <div class="grid-content bg-purple-light">
+          <el-button type="primary" @click="search" icon="el-icon-search">搜索</el-button>
+        </div>
+      </el-col>
+    </el-row>
+    <!--tree组件-->
     <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
-    <router-view/>
   </div>
 </template>
+<router-view/>
 
 <script>
 import axios from 'axios';
+
 export default {
   data() {
-    return {
+    return { // 在这里定义变量
+      wd:"",
+      input: '',
       data: [],
       defaultProps: {
         children: 'treeMenu',
-        label: 'name'
+        label: 'menuName'
       }
     };
   },
   methods: {
     handleNodeClick(data) {
       console.log(data);
-    }
+    },
+    search() {
+      axios.get('http://localhost:8080/treeMenu/selectByWd?wd='+this.wd)
+          .then(response => {
+            console.log(response);
+        this.data = response.data.data.items
+      })
+        .catch(error => {
+            console.log(error);
+          });
+    },
   },
   created() {
     axios.get('http://localhost:8080/treeMenu/selectAllWithTree')
