59d4ca36a52212e3ca0ccfbb5aefb1074b328b5e
Sally <1564358705@qq.com>
2023-06-27 18:06:14 +0800

    vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,17 @@
 module.exports = {
-    devServer : { // onBeforeSetupMiddleware : require('./mock/index.js'),
+    devServer : {
         proxy: {
-            "/treeMenu": {
-                target: "http://localhost:8080",
+            "/sys/user": {
+                target: "http://localhost:8081",
                 ws: true, // 如果要代理 websockets，配置这个参数
                 changeOrigin: true, // 跨域开关
-            }
+            },
+            // '/mock':{
+            //     target: 'http://localhost:8080',
+            //     ws: true,
+            //     changeOrigin: true,
+            // }
         }
     }
 }
+
