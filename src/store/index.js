a9b25fc825451e6283b407069f8aba4165a98a93
Sally <1564358705@qq.com>
2023-07-10 15:51:59 +0800

    定义和管理应用程序的状态

--- src/store/index.js
+++ src/store/index.js
@@ -3,18 +3,22 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-    menus: [] // 用于保存菜单数据
-  },
-  getters: {
-    menus: state => state.menus // 获取菜单数据
-  },
-  mutations: {
-    setMenus: (state, menus) => (state.menus = menus) // 设置菜单数据
-  },
-  actions: {
-  },
-  modules: {
-  }
+const store = new Vuex.Store({
+    // 定义应用程序的初始状态,state里面必须有原始值才能响应,新添加的属性不能响应修改,所以给个null
+    state: {
+        token: null,
+        roles: [],
+        currIndex: null
+    },
+    // 可以修改状态的方法(响应式)
+    mutations: {
+        loginIn(state, token, roles) {
+            state.token = token;
+            state.roles = roles;
+        },
+        setCurrIndex(state, currIndex) {
+            state.currIndex = currIndex
+        }
+    }
 })
+export default store;
