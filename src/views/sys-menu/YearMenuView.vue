79432814c16e8c3a62de5729c8ad28aa8add7688
Sally <1564358705@qq.com>
2023-07-10 15:54:15 +0800

    年度菜单

--- src/views/sys-menu/YearMenuView.vue
+++ src/views/sys-menu/YearMenuView.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>
-    <h1>Year Menu View</h1>
+  <div id="app">
+    <!--搜索框开始-->
+    <el-row>
+      <el-col :span="12">
+        <div class="grid-content bg-purple">
+          <el-input v-model="searchParam" placeholder="请输入名称"></el-input>
+        </div>
+      </el-col>
+      <el-col :span="1">
+        <div class="grid-content bg-purple-light">
+          <el-button type="primary" @click="handleSearch" icon="el-icon-search">搜索</el-button>
+        </div>
+      </el-col>
+    </el-row>
+    <!--tree组件-->
     <el-tree
-        :data="menuItems"
-        :props="{ label: 'menuName'}"
-    ></el-tree>
+        node-key="id"
+        lazy
+        :load="loadNode"
+        :props="defaultProps"
+        v-loading="list.loading"
+        :data="treeData"
+        ref="tree">
+    </el-tree>
+    <component :is="currentMenuView"></component>
   </div>
 </template>
 
@@ -12,16 +31,69 @@
 import axios from "axios";
 
 export default {
-  name: "YearMenuView",
   data() {
-    return {
-      menuItems: []
+    return { // 在这里定义变量
+      searchParam: "",
+      list: {
+        loading: false,
+        isExpand: false
+      },
+      defaultProps: { // tree 控件的数据结构，需要设置 isLeaf
+        children: 'children',
+        label: 'menuName',
+        isLeaf: 'isLeaf'
+      },
+      loading: false,
+      treeData: [],
+      currentMenuView: null,
     };
   },
-  async created() {
-    const res = await axios.get('/sys/menu/year');
-    this.menuItems = res.data.data.menus;
+  created() {
+    this.setUserRoleMenuView();
   },
+  methods: {
+    async loadNode(node, resolve) { // 懒加载时触发
+      if (node.level === 0) {
+        return resolve(await this.getRootItems())
+      } else {
+        return resolve(await this.getChildren(node.data.id))
+      }
+    },
+    async getRootItems() {
+      this.list.loading = true // 在这里进行mock测试，数据为mock.json中从后端拿回来的数据
+      const yearMenuId = 100;
+      let url = `http://localhost:8081/treeMenu/menus/${yearMenuId}`;
+      const res = await axios.get(url); // const res = mock;
+      const data = res.data.data.menus; // const data = res.data.menus;
+      data.forEach((item, index) => {
+        item.id = item.menuId
+      })
+      this.list.loading = false;
+      return data
+    },
+
+    async getChildren(menuId) {
+      let url = `http://localhost:8081/treeMenu/${menuId}`;
+      const res = await axios.get(url);
+      const data = res.data.data.menus;
+      data.forEach((item, index) => {
+        item.id = item.menuId
+      })
+      return data
+    },
+
+    async handleSearch() {
+      // 根据参数判断是否开启懒加载
+      this.$refs.tree.$data.store.lazy = !this.searchParam
+      const yearMenuId = 100;
+      let url = `http://localhost:8081/treeMenu/menus/search/${yearMenuId}/${this.searchParam}`;
+      if (this.searchParam) {
+        const res = await axios.get(url);
+        const data = res.data.data.menus;
+        this.treeData = data
+      }
+    }
+  }
 };
 </script>
 
